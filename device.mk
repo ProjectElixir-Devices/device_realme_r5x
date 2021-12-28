08cdc0ac01f71a61ed96aa59d8c71a441d8c98fb
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-12-28 13:08:56 +0000

    r5x: Remove references for antradio_library

--- device.mk
+++ device.mk
@@ -42,12 +42,6 @@ PRODUCT_SOONG_NAMESPACES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    antradio_app \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
